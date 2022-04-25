a40dc49185d5fbe5d9078efb0b5bcb3811f45474
Joanna Doudeva <jobox18@gmail.com>
2022-04-25 13:36:46 +0300

    Header, Footer, Sidebar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,17 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
