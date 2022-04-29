0e9988749d0121bacaadf2306d144111af3b90b7
Joanna Doudeva <jobox18@gmail.com>
2022-04-29 07:06:16 +0300

    Env endpoints to customer

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -2,6 +2,10 @@ export const environment = {
     production: false,
     CUSTOMER_BASE_URL: {
         BASE_URL: 'http://localhost:3000',
-        GET_ALL_CUSTOMERS: 'list'
+        GET_ALL_CUSTOMERS: 'list',
+        GET_CUSTOMER:'view',
+        EDIT_CUSTOMER:'edit',
+        DELETE_CUSTOMER:'delete',
+        SEARCH_CUSTOMER:'view',
     }
   };
