de1ed2e94686acfcaf63eb858706f670ed416242
Joanna Doudeva <jobox18@gmail.com>
2022-04-29 23:07:24 +0300

    Register controler and some stupid pistures

--- client/src/app/auth/new-user/new-user.component.ts
+++ client/src/app/auth/new-user/new-user.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { emailValidator } from '../util';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.css']
+  styleUrls: ['./new-user.component.css'],
 })
 export class NewUserComponent implements OnInit {
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, emailValidator]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    passwords: new FormGroup({
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      rePassword: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    }),
+  });
 
-  title = 'Tic Tac Toe';
-  public isAuthenticated = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  ngOnInit(): void {}
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-
+  handleRegister(): void {
+    console.log(this.registerFormGroup);
+    this.userService.login();
+    this.router.navigate(['/dashboard']);
   }
-
 }
