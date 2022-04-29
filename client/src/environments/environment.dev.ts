d52ab61089805fd4db879e2adae814eefea2b961
Joanna Doudeva <jobox18@gmail.com>
2022-04-29 07:10:07 +0300

    Loan end points in env

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -1,11 +1,19 @@
 export const environment = {
     production: false,
+    BASE_URL: 'http://localhost:3000',
+
     CUSTOMER_BASE_URL: {
-        BASE_URL: 'http://localhost:3000',
         GET_ALL_CUSTOMERS: 'list',
         GET_CUSTOMER:'view',
         EDIT_CUSTOMER:'edit',
         DELETE_CUSTOMER:'delete',
         SEARCH_CUSTOMER:'view',
+    },
+    LOAN: {
+        GET_ALL_LOANS: 'list',
+        GET_LOAN:'view',
+        EDIT_LOAN:'edit',
+        DELETE_LOAN:'delete',
+        SEARCH_LOAN:'view',
     }
   };
