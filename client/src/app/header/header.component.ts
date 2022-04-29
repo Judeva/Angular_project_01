19828ee3a1ec2c3435da61187b9e8555b786423d
Joanna Doudeva <jobox18@gmail.com>
2022-04-29 19:43:02 +0300

    Header for logged in user

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLoggedIn!: boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.isLoggedIn=false;
   }
 
 }
